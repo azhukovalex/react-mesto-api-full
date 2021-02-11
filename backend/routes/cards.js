f9b6b2330f4a3ec012b801aabe224fd0195483b2
azhukovalex <zhukov.alexey2017@gmail.com>
2021-02-11 22:44:25 +0300

    fix func like

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -10,8 +10,8 @@ const {
 
 cardsRouter.get('/cards', getCards);
 cardsRouter.post('/cards', validateCard, createCard);
-cardsRouter.delete('/cards/:id', validateId, deleteCard);
-cardsRouter.put('/cards/:id/likes', validateId, likeCard);
+cardsRouter.delete('/cards/:id', validateId, deleteCard); // id
+cardsRouter.put('/cards/:id/likes', validateId, likeCard); // id
 cardsRouter.delete('/cards/:id/likes', validateId, dislikeCard);
 
 module.exports = cardsRouter;
